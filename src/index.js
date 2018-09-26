5dc3810db32c82336c80836467ee5420ea20683b
Richard B <C:\Users\dmitr\AppData\Roaming\The Bat!>
2018-09-26 14:59:23 +0300

    eZ

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+ if (array.length == 0) {// your solution here
+  return 0;
+}
+
+array.sort((a, b) => a - b);
+
+let result = 1;
+let count = 1;
+let diference = 0;
+
+for (let i = 0; i < array.length - 1; i++) {
+  diference = array[i + 1] - array[i];
+    if (diference == 0) {
+      continue;
+    } else if (diference == 1) {
+        count++;
+        if (result < count) {
+          result = count;
+        }
+    } else {
+        count = 1;
+    }
+}
+return result;
 }
